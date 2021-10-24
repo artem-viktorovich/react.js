1d61f5c843a63a1dc6aeedfda9ae8da22dd9a9ec
artem-viktorovich <artemkavasechko@yandex.ru>
2021-10-24 11:41:10 +0300

    create-react-app

---
+++ create-react-app/11_lesson/src/Header.js
@@ -0,0 +1,13 @@
+let Header = () => {
+ return (
+	<header className='header'>
+			<div className='logo'>
+				<img src="https://e7.pngegg.com/pngimages/197/457/png-clipart-round-multicolored-logo-vanamo-logo-icons-logos-emojis-tech-companies.png" alt="logo" />
+			</div>
+		</header>
+ )
+}
+
+export default Header;
+
+
---
+++ create-react-app/11_lesson/src/Navigation.js
@@ -0,0 +1,19 @@
+let Navigation = () => {
+	return (
+		<section className='nav'>
+		<div class="header__menu menu">
+			<nav class="menu__body">
+				<ul class="menu__list">
+					<li class="menu__item"><a href="#" class="menu__link">Profile</a></li>
+					<li class="menu__item"><a href="#" class="menu__link">Messages</a></li>
+					<li class="menu__item"><a href="#" class="menu__link">News</a></li>
+					<li class="menu__item"><a href="#" class="menu__link">Music</a></li>
+					<li class="menu__item"><a href="#" class="menu__link">Settings</a></li>
+				</ul>
+			</nav>
+		</div>
+	</section>
+	)
+  }
+
+  export default Navigation;
